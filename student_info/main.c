34bc97fd01fdfa470966dd44a22c8325912c11ba
Your Name <obishoy6@gmail.com>
2021-04-18 14:06:22 +0200

    first part of the assignment i created the student struct and wrote the welcome message in the main function

---
+++ student_info/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct studen{
+    char student_name[20];
+    int student_id;
+    int student_birth_day;
+    int student_birth_month;
+    int student_birth_year;
+    int student_last_score;
+
+};
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
